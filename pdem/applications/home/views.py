060bb472909f6d30c6642a6a292680d2a461b6f3
Israel González <isra.igp@gmail.com>
2021-03-22 22:58:38 -0600

    Products page added with filter functionality, search bar working and products carousel also working

--- pdem/applications/home/views.py
+++ pdem/applications/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 
 from applications.category.models import Category
-from applications.product.models import Product
+from applications.product.models import Product, Variant
 
 class Home(ListView):
     context_object_name = 'category_list'
@@ -19,8 +19,17 @@ class Home(ListView):
 class Products(ListView):
     context_object_name = 'products_list'
     model = Product
+    paginate_by = 10
     template_name = 'home/products.html'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the top products
+        context['variants_list'] = Variant.objects.all()
+        return context
+
     def get_queryset(self):
-        # context = Product.objects.filter(category_id=1)
-        return []
+        kword = self.request.GET.get('kword', '')
+        context = Product.objects.filter(name__icontains=kword)
+        return context
