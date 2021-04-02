8e91c87a1f770a0302af4529b8dd830660c81f51
Israel González <isra.igp@gmail.com>
2021-04-02 12:28:24 -0600

    Added prod settings

---
+++ pdem/pdem/settings/prod.py
@@ -0,0 +1,44 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR.child('static')]
+AWS_ACCESS_KEY_ID = get_secret('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_secret('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = get_secret('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = get_secret('AWS_S3_CUSTOM_DOMAIN')
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'pdem.storage_backends.MediaStorage' #the media storage configurations
+
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR.child('media')
