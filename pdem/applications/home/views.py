7aa374e9ca1210dff9f3ee26b4c988683c8394ea
Israel González <isra.igp@gmail.com>
2021-04-15 20:36:55 -0500

    Media queries changed for product cards height

--- pdem/applications/home/views.py
+++ pdem/applications/home/views.py
@@ -19,7 +19,7 @@ class Home(ListView):
 class Products(ListView):
     context_object_name = 'products_list'
     model = Product
-    paginate_by = 1
+    paginate_by = 2
     template_name = 'home/products.html'
 
     def get_context_data(self, **kwargs):
